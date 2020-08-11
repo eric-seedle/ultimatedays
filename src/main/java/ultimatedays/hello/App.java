ec5f35262576f0ff9e2f60cde269eb90ac090490
eric <eric@eric-Sabre-15GV8>
2020-08-11 10:56:25 +0200

    Suite intervention de Paola

--- src/main/java/ultimatedays/hello/App.java
+++ src/main/java/ultimatedays/hello/App.java
@@ -7,5 +7,11 @@ package ultimatedays.hello;
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World! Avec ma modification  - test - test 2 - test 3 - test 5");
+	
+		System.out.println(getValue());
+	}
+	
+	public static String getValue() {
+		return "Coucou Paola";
 	}
 }
