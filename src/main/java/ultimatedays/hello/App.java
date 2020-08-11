2ef0c6ad3ed342e253171b34d6d15b49d8c406bf
eric <eric@eric-Sabre-15GV8>
2020-08-11 10:47:00 +0200

    Test 2

--- src/main/java/ultimatedays/hello/App.java
+++ src/main/java/ultimatedays/hello/App.java
@@ -6,6 +6,6 @@ package ultimatedays.hello;
  */
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World! Avec ma modification  - test");
+		System.out.println("Hello World! Avec ma modification  - test - test 2");
 	}
 }
