f4f3c944731640818673acac3b44979653ead960
eric <eric@eric-Sabre-15GV8>
2020-08-11 14:34:54 +0200

    Test suite à la mise en oeuvre de Jenkins

--- src/main/java/ultimatedays/hello/App.java
+++ src/main/java/ultimatedays/hello/App.java
@@ -7,11 +7,17 @@ package ultimatedays.hello;
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World! Avec ma modification  - test - test 2 - test 3 - test 5");
-	
+
 		System.out.println(getValue());
+		System.out.println(getValue("tous !!!!!"));
+
 	}
-	
+
 	public static String getValue() {
 		return "Coucou Paola";
+	}
+	
+	public static String getValue(String in) {
+		return "Coucou à " + in;
 	}
 }
